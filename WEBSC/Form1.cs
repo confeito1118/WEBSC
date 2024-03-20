ec6788853c0fb52d7cfa4af578dff7c39ca4938a
owl <reltsuh1118@gmail.com>
2024-03-20 14:38:38 +0900

    test

--- WEBSC/Form1.cs
+++ WEBSC/Form1.cs
@@ -1,3 +1,5 @@
+using HtmlAgilityPack;
+
 namespace WEBSC
 {
     public partial class Form1 : Form
@@ -5,6 +7,17 @@ namespace WEBSC
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // the URL of the target Wikipedia page
+            string url = "https://en.wikipedia.org/wiki/List_of_SpongeBob_SquarePants_episodes";
+
+            var web = new HtmlWeb();
+            // downloading to the target page
+            // and parsing its HTML content
+            var document = web.Load(url);
         }
     }
 }
